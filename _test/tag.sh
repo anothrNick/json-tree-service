2e538690b894772c7730b3f22147d65f60e3d2e4
anothrNick <nick.sjostrom12@gmail.com>
2019-08-28 16:51:42 -0400

    new test commit. #major

--- _test/tag.sh
+++ _test/tag.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 # get latest tag
-t=git describe --tags `git rev-list --tags --max-count=1` > /dev/null 2>&1
+t=$(git describe --tags `git rev-list --tags --max-count=1`) > /dev/null 2>&1
 
+# if there are none, start tags at 0.0.0
 if [ -z "$t" ]
 then
-      t=0.0.0
+    log=$(git log --pretty=oneline)
+    t=0.0.0
+else
+    log=$(git log $t..HEAD --pretty=oneline)
 fi
 
 echo $t
+
+case "$log" in
+    *#major* ) echo "major bump";;
+    *#patch* ) echo "patch bump";;
+    * ) echo "minor bump";;
+esac
